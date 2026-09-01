1f19b9eb7dbd2b11f2a7834d4ba956f35f23738f
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-09-01 12:27:57 +0600

    cleanup(calculator): remove standalone EMI calculator loading skeleton

--- src/app/(main)/calculator/loading.tsx
+++
@@ -1,5 +0,0 @@
-// Pattern for loading.tsx (customize per route)
-import { PageSkeleton } from "@/components/feedback/LoadingSkeleton";
-export default function Loading() {
-	return <PageSkeleton />;
-}
