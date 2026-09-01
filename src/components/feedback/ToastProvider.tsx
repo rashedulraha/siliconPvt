693a24078b4a9f1f12797a3024f439cc82c68de7
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-09-01 12:28:03 +0600

    refactor(feedback): optimize ToastProvider alerts

--- src/components/feedback/ToastProvider.tsx
+++ src/components/feedback/ToastProvider.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { Toaster } from "sonner";
-import { useTheme } from "next-themes";
 
 export function ToastProvider() {
-	const { theme } = useTheme();
-
 	return (
 		<Toaster
-			theme={theme as "light" | "dark" | "system"}
+			theme="light"
 			position="bottom-right"
 			toastOptions={{
 				style: {
